6dad8e1b2e4f1b7d90a11e7797619c0401513e44
GaSoler <gabriel.edsoler@gmail.com>
2024-04-05 08:16:14 -0300

    .

--- server/src/routes/memories.ts
+++ server/src/routes/memories.ts
@@ -10,7 +10,7 @@ export async function memoriesRoutes(app: FastifyInstance) {
   app.get('/memories', async (request) => {
     const memories = await prisma.memory.findMany({
       where: {
-        userId: request.user.sub,
+        userId: request.user.id,
       },
       orderBy: {
         createdAt: 'asc'
@@ -40,7 +40,7 @@ export async function memoriesRoutes(app: FastifyInstance) {
       },
     })
 
-    if (memory.userId !== request.user.sub) {
+    if (memory.userId !== request.user.id) {
       return reply.status(401).send()
     }
 
@@ -61,7 +61,7 @@ export async function memoriesRoutes(app: FastifyInstance) {
         content,
         coverUrl,
         createdAt,
-        userId: request.user.sub,
+        userId: request.user.id,
       },
     })
 
@@ -88,7 +88,7 @@ export async function memoriesRoutes(app: FastifyInstance) {
       },
     })
 
-    if (memory.userId !== request.user.sub) {
+    if (memory.userId !== request.user.id) {
       return reply.status(401).send()
     }
 
@@ -118,7 +118,7 @@ export async function memoriesRoutes(app: FastifyInstance) {
       },
     })
 
-    if (memory.userId !== request.user.sub) {
+    if (memory.userId !== request.user.id) {
       return reply.status(401).send()
     }
 
